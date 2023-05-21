2a2d5f439214ae5bae50b8a4a3ec2426b76099e8
WunnaThaw <wunna.pe@gmail.com>
2023-05-21 20:53:45 +0630

    Singleton Design Pattern

---
+++ Singleton/DBInstance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class DBInstance
+    {
+        protected static DBInstance _obj;
+        private DBInstance()
+        {
+
+        }
+        public static DBInstance GetDBInstance()
+        {
+            if (_obj == null)
+            {
+                _obj = new DBInstance();
+            }
+            return _obj;
+        }
+        public void Print(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
